a55a564457a7513621be7cc282ec478681001d34
rihala <rihala345@gmail.com>
2024-05-23 13:17:00 +0530

    js

---
+++ Pattern.js
@@ -0,0 +1,24 @@
+rows=6
+b=1
+for (i=1; i<=6; i++) {
+    a=""
+    for (j=1; j<i;j++){
+        i=i*1
+        // j=j*1
+        
+        // a=(a+"*");
+        console.log(b);
+
+
+        // i++
+        // console.log(i);
+    }
+    for(k=0; k<=6; k++){
+       
+
+    }
+   console.log("*"+i*i+"*"); 
+}
+for(i=1;i<=6;i--){
+    a=""
+}
---
+++ Rightangle.js
@@ -0,0 +1,25 @@
+rows = 6
+
+
+for (i=1; i<=rows; i++) {
+    a=""
+    for (j=1; j<=i; j++){
+        
+        // if (i==0) {
+        //     console.log("*");
+        // }
+        // else (i==1) {
+        
+           a=a+"* ";
+
+    } 
+    console.log(a)  
+
+            
+        
+   
+       
+
+
+    
+}
